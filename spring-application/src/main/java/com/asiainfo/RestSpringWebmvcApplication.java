305b0c3ab48109c9680eeb9d759011ba8c2ea7db
qianminglang <569040804@qq.com>
2019-04-01 20:37:46 +0800

    Initial commit

---
+++ spring-application/src/main/java/com/asiainfo/RestSpringWebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.asiainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringWebmvcApplication.class, args);
+	}
+
+}
